5e5299508e3a0cfbe4adf3c31fe3dc3ce0708ecc
ogg <ogg@purdue.edu>
2014-12-13 14:36:11 -0500

    Added Purdue AI

---
+++ src/com/nullprogram/chess/puai/PUMiniMax.java
@@ -0,0 +1,16 @@
+package com.nullprogram.chess.puai;
+
+import com.nullprogram.chess.Board;
+import com.nullprogram.chess.Move;
+import com.nullprogram.chess.Piece.Side;
+import com.nullprogram.chess.Player;
+
+public class PUMiniMax implements Player {
+
+	@Override
+	public Move takeTurn(Board board, Side side) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
